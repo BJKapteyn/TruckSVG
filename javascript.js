2d30c4555b7ce5040e6565b01799cd505d74053f
Brad <bjkapteyn@gmail.com>
2019-09-19 14:34:27 -0400

    started adding a tags to all groups for clickable links

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let kenworthParts = {
+    1: document.getElementById(),
+    2: document.getElementById(),
+    3: document.getElementById(),
+    4: document.getElementById(),
+    5: document.getElementById(),
+    6: document.getElementById(),
+    7: document.getElementById()
+}
